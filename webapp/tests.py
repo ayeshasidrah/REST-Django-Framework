e018e8e6363d52dcb3601458981a0bce32e704bf
ayeshasidrah <74006665+ayeshasidrah@users.noreply.github.com>
2021-06-30 20:06:12 +0530

    Update tests.py

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework import status
+from . models import mobiles
+from . serializers import mobilesSerializer
+import requests
+import pytest
+import json
 
-# Create your tests here.
+def test_get_name():
+    response = requests.get("http://localhost:8000/mobiles/")
+    assert response.status_code == 200
+
+
+def test_get_performances():
+    response = requests.get("http://localhost:8000/mobiles/")
+    response_body = response.json()
+    assert response_body["performance"] == "excellent"
+
